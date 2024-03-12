783fe1b07278fb423348cee9b7043a6ae05136a8
phuc <phuc.dongsi@gmail.com>
2024-03-12 15:45:22 +0700

    fix file

--- src/services/roomService.js
+++ src/services/roomService.js
@@ -1,20 +1,8 @@
-import axios from "axios";
-import apiClients from "../apiClients/apiClients";
-
-class RoomService {
-
-  static async getAllRooms(url) {
+export default class RoomService {
+  async getAllRoom(url) {
     return fetch(url)
       .then((res) => res.json())
-  }
-  static async createRoom(formData) {
-    console.log("formData", formData);
-    // return axios.post(url, formData, {
-    //   headers: {
-    //     'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
-    //   },
-    // });
-    return apiClients.post('/rooms',formData)
+      .then((data) => data)
+      .catch((error) => error);
   }
 }
-export default RoomService;

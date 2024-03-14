05710a4527859d759a592d4914de17d0507e8984
datnguyeno1997 <tn6248293@gmail.com>
2024-03-14 08:10:39 +0700

    up

--- src/pages/BookPage/BookingCheckoutPage.js
+++ src/pages/BookPage/BookingCheckoutPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import BookingCheckouts from "../../components/home/BookingCheckout";
+import BookingCheckouts from "../../components/home/Booking/BookingCheckout";
 
 export default function Booking() {
   return (
--- src/services/roomService.js
+++ src/services/roomService.js
@@ -5,8 +5,22 @@ class RoomService {
     return apiClients.get(`/rooms`);
   }
   // get all room
+
+  
   static async getAllRooms(url) {
     return fetch(url)
+      .then((res) => res.json())
+      .then((data) => data)
+      .catch((error) => error);
+  }
+  static async searchRooms(url, objSend) {
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(objSend),
+    })
       .then((res) => res.json())
       .then((data) => data)
       .catch((error) => error);

be937bebe73318f41b6b1c78794d5e5a4bde74f9
quangdangcodegym <quang.dang@codegym.vn>
2024-03-23 08:51:43 +0700

    update

--- src/services/roomService.js
+++ src/services/roomService.js
@@ -1,14 +1,8 @@
-import apiClients from "../apiClients/apiClients";
-//up
-class RoomService {
-  static getRoomList() {
-    return apiClients.get(`/rooms`);
-  }
-  static async getAllRooms(url) {
+export default class RoomService {
+  async getAllRoom(url) {
     return fetch(url)
       .then((res) => res.json())
       .then((data) => data)
       .catch((error) => error);
   }
 }
-export default RoomService;

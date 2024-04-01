25a78be9587320d4ef696e504977b01a7f142559
Thang <nguyentrongthang10051999@gmail.com>
2024-04-01 19:31:16 +0700

    Phan Quyen - Dang nhap

--- src/components/dashboard/rooms/RoomList.jsx
+++ src/components/dashboard/rooms/RoomList.jsx
@@ -25,7 +25,8 @@ import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import RoomRealService from "../../../services/RoomRealService";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import VisibilityIcon from '@mui/icons-material/Visibility';
-
+import useAuth from "../../common/UseAuth";
+import { useNavigate } from "react-router-dom";
 
 export default function RoomList() {
     const [roomList, setRoomList] = useState([]);
@@ -45,7 +46,8 @@ export default function RoomList() {
     })
     const [totalPages, setTotalPages] = useState(0)
     const [roomTypeList, setRoomTypeList] = useState([]);
-    const [keyword, setKeyword] = useState(null)
+    const [keyword, setKeyword] = useState(null);
+    const navigate = useNavigate();
     const [selectDate, setSelectDate] = useState([
         dayjs(),
         dayjs().add(1, 'day')
@@ -62,6 +64,8 @@ export default function RoomList() {
     const [loadDataRoomReal, setLoadDataRoomReal] = useState(false)
     const [dataRoomRealUnAvailableFind, setDataRoomRealUnAvailableFind] = useState([])
     const [loadDataRoomRealUnAvailable, setLoadDataRoomRealUnAvailable] = useState(false)
+
+    const { user, hasAnyRole } = useAuth();
     function calculateUrl(filters) {
         let urlArray = [];
         if (filters.kw !== "") {
@@ -453,7 +457,7 @@ export default function RoomList() {
                                                                                                             />
                                                                                                         </Link>
                                                                                                         <div className="mx-1">
-                                                                                                            <div onClick={() => handleShowModalEditRoom(room)} role="button" title="edit">
+                                                                                                            <div onClick={() => hasAnyRole(user.roles[0], ["ROLE_ADMIN"]) ? handleShowModalEditRoom(room) : navigate("/login")} role="button" title="edit">
                                                                                                                 <EditIcon
                                                                                                                     style={{ color: 'green' }}
                                                                                                                     size={22}
